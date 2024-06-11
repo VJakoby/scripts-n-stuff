727ed402911cca07fbb0ac0f67a43a82bb68c8e3
Jakob Vango (Work) <jakobwango@protonmail.com>
2024-06-11 13:46:58 +0200

    tetet

--- Linux-apps.sh
+++ Linux-apps.sh
@@ -2,38 +2,38 @@
 
 # Install apps
 install_kali_apps() {
-    printf "[*] Installing Kali specific apps only..."
+    printf "[*] Installing Kali specific apps only...\n"
 
     # RustScan
-    printf "[*] Downloading and installing RustScan..."
+    printf "[*] Downloading and installing RustScan...\n"
 
     wget https://github.com/RustScan/RustScan/releases/download/2.2.3/rustscan_2.2.3_amd64.deb
-    sudo dpkg -i rustscan_*
-    sudo apt-get install -f
+    sudo dpkg -i rustscan_* || exit
+    sudo apt-get install -f || exit
 
-    printf "[*] Installing git-dumper..."
-    pip install git-dumper
+    printf "[*] Installing git-dumper...\n"
+    pip install git-dumper || exit
 
     printf "[*] Installing rEngine..."
-    git clone https://github.com/yogeshojha/rengine $HOME/rengine
+    git clone https://github.com/yogeshojha/rengine $HOME/rengine || exit
     cd rengine || exit
 
-    printf "[*] Don't forget to edit the file /rengine/.env .."
+    printf "[*] Don't forget to edit the file /rengine/.env ...\n"
 }
 
 install_i3_desktop() {
 
-    printf "[*] Installing i3, polybar, nitrogen, rofi..."
-    sudo apt install -y i3 polybar nitrogen rofi
+    printf "[*] Installing i3, polybar, nitrogen, rofi...\n"
+    sudo apt install -y i3 polybar nitrogen rofi || exit
 
-    printf "[*] Downloading Polybar themes..."
-    git clone --depth=1 https://github.com/adi1090x/polybar-themes.git $HOME/polybar-themes
+    printf "[*] Downloading Polybar themes...\n"
+    git clone --depth=1 https://github.com/adi1090x/polybar-themes.git $HOME/polybar-themes || exit
     cd $HOME/polybar-themes || exit
-    chmod +x setup.sh
+    chmod +x setup.sh || exit
     
-    printf "[*] Copying i3 configs..."
-    mkdir -p $HOME/.config/i3
-    cp i3/config $HOME/.config/i3/
+    printf "[*] Copying i3 configs...\n"
+    mkdir -p $HOME/.config/i3 || exit
+    cp i3/config $HOME/.config/i3/ || exit
 }
 
 main() {
